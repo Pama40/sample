45b65c4b74f30da7f5c412cfa4ec98c9677764b1
padma <amozeenpama@gmail.com>
2024-03-14 10:35:15 +0530

    sample

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,12 @@ from flask_login import LoginManager
 # Create Flask application instance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sample.db'
+
 
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
+app.app_context().push()
 
 # Initialize Flask-Login
 login_manager = LoginManager()
@@ -18,4 +20,6 @@ login_manager.init_app(app)
 from app import views, models
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.models import User
@@ -7,6 +7,7 @@ from app.forms import LoginForm, RegistrationForm
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,7 +18,8 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user and user.check_password(form.password.data):
             login_user(user)
-            return redirect(url_for('welcome'))
+            next_page = request.args.get('next')  # Get the next page from the URL query parameters
+            return redirect(next_page or url_for('welcome'))  # Redirect to the next page if provided, else to the welcome page
     return render_template('login.html', form=form)
 
 @app.route('/logout')
@@ -48,3 +50,17 @@ def welcome():
 def generate_recipe():
     # Your code to handle generating recipes from ingredients
     return render_template('generate_recipe.html')
+
+@app.route('/profile')
+@login_required
+def profile():
+    # Logic to fetch and display user profile information
+    return render_template('profile.html')
+
+@app.route('/choose_option')
+@login_required
+def choose_option():
+    # This page allows users to choose between diet and recipe options
+    return render_template('choose_option.html')
+
+# You can add more routes as needed for additional functionalities
